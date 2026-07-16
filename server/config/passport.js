91959e368c1b5aa18f3d0056a005b583b354d830
somu325 <somureddyirikireddy@gmail.com>
2026-07-16 13:02:22 +0530

    feat: update site favicon to PNG and dynamicize OAuth callback URLs using environment variables

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,7 @@ const oauthService = require('../services/oauthService');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/api/auth/google/callback'
+    callbackURL: process.env.SERVER_URL + '/api/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
@@ -32,7 +32,7 @@ passport.use(new GoogleStrategy({
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:5000/api/auth/github/callback'
+    callbackURL: process.env.SERVER_URL + '/api/auth/github/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
